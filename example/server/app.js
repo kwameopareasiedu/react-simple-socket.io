1c7678938802324139e005ef0194e76ec6ff3b65
Kwame Opare Asiedu <asieduoparekwame@gmail.com>
2022-11-05 14:29:55 +0000

    Added example server

---
+++ example/server/app.js
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const app = express();
+
+app.use(logger("react-socket.io-server"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+module.exports = app;
